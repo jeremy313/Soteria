bc4af42dad97358b9588565e178d6d903a7d2209
Jingwei Sun <js905@hl279-cmp-03.egr.duke.edu>
2021-03-28 04:33:16 +0000

    clean GS attack

--- GS_attack/reconstruct_image.py
+++ GS_attack/reconstruct_image.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
             model = torchvision.models.resnet18(pretrained=args.trained_model)
         model_seed = None
     else:
-        model, model_seed = inversefed.construct_model(args.model, num_classes=10, num_channels=1)
+        model, model_seed = inversefed.construct_model(args.model, num_classes=10, num_channels=3)
     model.to(**setup)
     model.eval()
 
@@ -59,47 +59,27 @@ if __name__ == "__main__":
     # print(f'Val loss is {training_stats["valid_losses"][-1]:6.4f}, Val {name}: {training_stats["valid_" + name][-1]:{format}}.')
 
     # Choose example images from the validation set or from third-party sources
-    if args.num_images == 1:
-        if args.target_id == -1:  # demo image
-            # Specify PIL filter for lower pillow versions
-            ground_truth = torch.as_tensor(np.array(Image.open("auto.jpg").resize((32, 32), Image.BICUBIC)) / 255, **setup)
-            ground_truth = ground_truth.permute(2, 0, 1).sub(dm).div(ds).unsqueeze(0).contiguous()
-            if not args.label_flip:
-                labels = torch.as_tensor((1,), device=setup['device'])
-            else:
-                labels = torch.as_tensor((5,), device=setup['device'])
-            target_id = -1
-            print(ground_truth.shape)
+    if args.target_id == -1:  # demo image
+        # Specify PIL filter for lower pillow versions
+        ground_truth = torch.as_tensor(np.array(Image.open("auto.jpg").resize((32, 32), Image.BICUBIC)) / 255, **setup)
+        ground_truth = ground_truth.permute(2, 0, 1).sub(dm).div(ds).unsqueeze(0).contiguous()
+        if not args.label_flip:
+            labels = torch.as_tensor((1,), device=setup['device'])
         else:
-            if args.target_id is None:
-                target_id = np.random.randint(len(validloader.dataset))
-            else:
-                target_id = args.target_id
-            ground_truth, labels = validloader.dataset[target_id]
-            if args.label_flip:
-                labels = torch.randint((10,))
-            ground_truth, labels = ground_truth.unsqueeze(0).to(**setup), torch.as_tensor((labels,), device=setup['device'])
-            print(ground_truth.shape)
-        img_shape = (1, ground_truth.shape[2], ground_truth.shape[3])
-
+            labels = torch.as_tensor((5,), device=setup['device'])
+        target_id = -1
+        print(ground_truth.shape)
     else:
-        ground_truth, labels = [], []
         if args.target_id is None:
             target_id = np.random.randint(len(validloader.dataset))
         else:
             target_id = args.target_id
-        while len(labels) < args.num_images:
-            img, label = validloader.dataset[target_id]
-            target_id += 1
-            if label not in labels:
-                labels.append(torch.as_tensor((label,), device=setup['device']))
-                ground_truth.append(img.to(**setup))
-
-        ground_truth = torch.stack(ground_truth)
-        labels = torch.cat(labels)
+        ground_truth, labels = validloader.dataset[target_id]
         if args.label_flip:
-            labels = torch.permute(labels)
-        img_shape = (3, ground_truth.shape[2], ground_truth.shape[3])
+            labels = torch.randint((10,))
+        ground_truth, labels = ground_truth.unsqueeze(0).to(**setup), torch.as_tensor((labels,), device=setup['device'])
+        print(ground_truth.shape)
+    img_shape = (3, ground_truth.shape[2], ground_truth.shape[3])
 
     # Run reconstruction
     if args.accumulation == 0:
@@ -137,113 +117,25 @@ if __name__ == "__main__":
                 thresh = np.percentile(flattened_weights, args.pruning_rate)
                 grad_tensor = np.where(abs(grad_tensor) < thresh, 0, grad_tensor)
                 input_gradient[i] = torch.Tensor(grad_tensor).to(**setup)
-        if args.defense == 'Gauss':
-            for i in range(len(input_gradient)):
-                grad_tensor = input_gradient[i].cpu().numpy()
-                noise = np.random.normal(0,args.noise_sigma, size=grad_tensor.shape)
-                grad_tensor = grad_tensor + noise
-                input_gradient[i] = torch.Tensor(grad_tensor).to(**setup)
-        if args.defense == 'Lap':
-            for i in range(len(input_gradient)):
-                grad_tensor = input_gradient[i].cpu().numpy()
-                noise = np.random.laplace(0,args.noise_sigma, size=grad_tensor.shape)
-                grad_tensor = grad_tensor + noise
-                input_gradient[i] = torch.Tensor(grad_tensor).to(**setup)
         full_norm = torch.stack([g.norm() for g in input_gradient]).mean()
         print(f'Full gradient norm is {full_norm:e}.')
-
-        # Run reconstruction in different precision?
-        if args.dtype != 'float':
-            if args.dtype in ['double', 'float64']:
-                setup['dtype'] = torch.double
-            elif args.dtype in ['half', 'float16']:
-                setup['dtype'] = torch.half
-            else:
-                raise ValueError(f'Unknown data type argument {args.dtype}.')
-            print(f'Model and input parameter moved to {args.dtype}-precision.')
-            dm = torch.as_tensor(inversefed.consts.cifar10_mean, **setup)[:, None, None]
-            ds = torch.as_tensor(inversefed.consts.cifar10_std, **setup)[:, None, None]
-            ground_truth = ground_truth.to(**setup)
-            input_gradient = [g.to(**setup) for g in input_gradient]
-            model.to(**setup)
-            model.eval()
-
-        if args.optim == 'ours':
-            config = dict(signed=args.signed,
-                          boxed=args.boxed,
-                          cost_fn=args.cost_fn,
-                          indices='def',
-                          weights='equal',
-                          lr=0.1,
-                          optim=args.optimizer,
-                          restarts=args.restarts,
-                          max_iterations=24_000,
-                          total_variation=args.tv,
-                          init='randn',
-                          filter='none',
-                          lr_decay=True,
-                          scoring_choice='loss')
-        elif args.optim == 'zhu':
-            config = dict(signed=False,
-                          boxed=False,
-                          cost_fn='l2',
-                          indices='def',
-                          weights='equal',
-                          lr=1e-4,
-                          optim='LBFGS',
-                          restarts=args.restarts,
-                          max_iterations=300,
-                          total_variation=args.tv,
-                          init=args.init,
-                          filter='none',
-                          lr_decay=False,
-                          scoring_choice=args.scoring_choice)
-
-        rec_machine = inversefed.GradientReconstructor(model, (dm, ds), config, num_images=args.num_images)
-        output, stats = rec_machine.reconstruct(input_gradient, labels, img_shape=img_shape, dryrun=args.dryrun)
-
-    else:
-        local_gradient_steps = args.accumulation
-        local_lr = 1e-4
-        input_parameters = inversefed.reconstruction_algorithms.loss_steps(model, ground_truth, labels,
-                                                                           lr=local_lr, local_steps=local_gradient_steps)
-        input_parameters = [p.detach() for p in input_parameters]
-
-        # Run reconstruction in different precision?
-        if args.dtype != 'float':
-            if args.dtype in ['double', 'float64']:
-                setup['dtype'] = torch.double
-            elif args.dtype in ['half', 'float16']:
-                setup['dtype'] = torch.half
-            else:
-                raise ValueError(f'Unknown data type argument {args.dtype}.')
-            print(f'Model and input parameter moved to {args.dtype}-precision.')
-            ground_truth = ground_truth.to(**setup)
-            dm = torch.as_tensor(inversefed.consts.cifar10_mean, **setup)[:, None, None]
-            ds = torch.as_tensor(inversefed.consts.cifar10_std, **setup)[:, None, None]
-            input_parameters = [g.to(**setup) for g in input_parameters]
-            model.to(**setup)
-            model.eval()
 
         config = dict(signed=args.signed,
                       boxed=args.boxed,
                       cost_fn=args.cost_fn,
-                      indices=args.indices,
-                      weights=args.weights,
-                      lr=1,
+                      indices='def',
+                      weights='equal',
+                      lr=0.1,
                       optim=args.optimizer,
                       restarts=args.restarts,
                       max_iterations=24_000,
                       total_variation=args.tv,
-                      init=args.init,
+                      init='randn',
                       filter='none',
                       lr_decay=True,
-                      scoring_choice=args.scoring_choice)
-
-        rec_machine = inversefed.FedAvgReconstructor(model, (dm, ds), local_gradient_steps, local_lr, config,
-                                                     num_images=args.num_images, use_updates=True)
-        output, stats = rec_machine.reconstruct(input_parameters, labels, img_shape=img_shape, dryrun=args.dryrun)
-
+                      scoring_choice='loss')
+    rec_machine = inversefed.GradientReconstructor(model, (dm, ds), config, num_images=args.num_images)
+    output, stats = rec_machine.reconstruct(input_gradient, labels, img_shape=img_shape, dryrun=args.dryrun)
 
     # Compute stats
     test_mse = (output - ground_truth).pow(2).mean().item()
@@ -257,9 +149,6 @@ if __name__ == "__main__":
         output_denormalized = torch.clamp(output * ds + dm, 0, 1)
         rec_filename = (f'{validloader.dataset.classes[labels][0]}_{"trained" if args.trained_model else ""}'
                         f'{args.model}_{args.cost_fn}-{args.target_id}-{args.defense}-{args.pruning_rate}.png')
-        if args.defense == 'Gauss' or args.defense == 'Lap':
-            rec_filename = (f'{validloader.dataset.classes[labels][0]}_{"trained" if args.trained_model else ""}'
-                            f'{args.model}_{args.cost_fn}-{args.target_id}-{args.defense}-{args.noise_sigma}.png')
         torchvision.utils.save_image(output_denormalized, os.path.join(args.image_path, rec_filename))
 
         gt_denormalized = torch.clamp(ground_truth * ds + dm, 0, 1)
@@ -272,36 +161,6 @@ if __name__ == "__main__":
 
     # Save to a table:
     print(f"Rec. loss: {stats['opt']:2.4f} | MSE: {test_mse:2.4f} | PSNR: {test_psnr:4.2f} | FMSE: {feat_mse:2.4e} |")
-
-    inversefed.utils.save_to_table(args.table_path, name=f'exp_{args.name}', dryrun=args.dryrun,
-
-                                   model=args.model,
-                                   dataset=args.dataset,
-                                   trained=args.trained_model,
-                                   accumulation=args.accumulation,
-                                   restarts=args.restarts,
-                                   OPTIM=args.optim,
-                                   cost_fn=args.cost_fn,
-                                   indices=args.indices,
-                                   weights=args.weights,
-                                   scoring=args.scoring_choice,
-                                   init=args.init,
-                                   tv=args.tv,
-
-                                   rec_loss=stats["opt"],
-                                   psnr=test_psnr,
-                                   test_mse=test_mse,
-                                   feat_mse=feat_mse,
-
-                                   target_id=target_id,
-                                   seed=model_seed,
-                                   timing=str(datetime.timedelta(seconds=time.time() - start_time)),
-                                   dtype=setup['dtype'],
-                                   epochs=defs.epochs,
-                                   val_acc=None,
-                                   rec_img=rec_filename,
-                                   gt_img=gt_filename
-                                   )
 
 
     # Print final timestamp
